08c1162e50e659779baa3f9830b0f761b8293241
AliasgharHasanzadeh <a.hasanzadeh2006@gmail.com>
2025-07-25 08:56:47 +0330

    update myWork

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const hamburger = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.nav-links');
 const links = document.querySelectorAll('.nav-links a');
-
+const container = document.querySelector('.container')
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
     navLinks.classList.toggle('active');
@@ -14,3 +14,28 @@ links.forEach(link => {
         navLinks.classList.remove('active');
     });
 }); 
+// console.log(container)
+async function updateMyWork() {
+    const res = await fetch('https://myproject.majid-samurai2006.workers.dev/')
+    const data = await res.json()
+    data.forEach(item=>{
+        const newDiv = document.createElement('div')
+        newDiv.innerHTML = `
+        <img src="${item.img}">
+          <h1>${item.title}</h1>
+          <p>${item.description}</p>
+          <p>${item.tech}</p>
+          <a href="${item.link}" target="_blank">
+            View Project
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+              <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
+              <polyline points="15 3 21 3 21 9"></polyline>
+              <line x1="10" y1="14" x2="21" y2="3"></line>
+            </svg>
+          </a>
+        `
+        container.appendChild(newDiv)
+         
+    })
+}
+updateMyWork()
